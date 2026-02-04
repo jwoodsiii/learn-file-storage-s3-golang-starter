05acc27bdc55d938e14dc0419f9618aac8969ec0
jwoodsiii <woods3.jessie@gmail.com>
2026-02-03 20:49:23 -0800

    adding upload video function, integrating with s3 client

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
@@ -31,4 +40,70 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to get video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User not authorized to access video", nil)
+		return
+	}
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+
+	mimeType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if mimeType != "video/mp4" || err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Content-type for video", nil)
+		return
+	}
+
+	ext := strings.Split(mimeType, "/")[1]
+
+	tmp, err := os.CreateTemp("", "tubey-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating tmp file", err)
+		return
+	}
+	defer os.Remove(tmp.Name())
+	defer tmp.Close()
+
+	_, err = io.Copy(tmp, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error copying file", err)
+		return
+	}
+
+	tmp.Seek(0, io.SeekStart)
+
+	path := make([]byte, 32)
+	rand.Read(path)
+
+	fileKey := base64.RawURLEncoding.EncodeToString(path)
+
+	cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:      aws.String(cfg.s3Bucket),
+		Key:         aws.String(fmt.Sprintf("%s/%s.%s", fileKey, videoID, ext)),
+		Body:        tmp,
+		ContentType: aws.String("video/mp4"),
+	})
+
+	// https://<bucket-name>.s3.<region>.amazonaws.com/<key>
+	url := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s/%s.%s", cfg.s3Bucket, cfg.s3Region, fileKey, videoID, ext)
+	video.VideoURL = &url
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
